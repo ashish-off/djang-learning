c9b285836e8b8a23ead5fc33332a7a306c2748ed
Ashish <ashishsaud498@gmail.com>
2025-06-25 23:24:20 +0545

    Integrated Tailwind CSS,  Set up hot reloading, enabled the Django admin panel.

--- ashishDjango/ashishDjango/settings.py
+++ ashishDjango/ashishDjango/settings.py
@@ -39,8 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # adding apps here
-    'ashish'
+    'ashish',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'ashishDjango.urls'
